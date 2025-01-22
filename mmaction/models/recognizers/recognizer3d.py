3ad8fd5d63acc349fe5992485e79d52cfc499984
bigyan <bigyaaan@gmail.com>
2025-01-22 21:18:23 +0545

    changes in recognizer3d

--- mmaction/models/recognizers/recognizer3d.py
+++ mmaction/models/recognizers/recognizer3d.py
@@ -15,7 +15,9 @@ class Recognizer3D(BaseRecognizer):
         assert self.with_cls_head
         imgs = imgs.reshape((-1, ) + imgs.shape[2:])
         losses = dict()
-        print(imgs)
+        print("//////////////////////////////////////////////////////////////////////////////////\n\n\n",imgs)
+        print("//////////////////////////////////\n\n\n",type(imgs))
+
         x = self.extract_feat(imgs)
         if self.with_neck:
             x, loss_aux = self.neck(x, labels.squeeze())
