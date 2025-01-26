09b39946959868e332d017a98bece857520e0efd
bigyan <bigyaaan@gmail.com>
2025-01-26 14:21:43 +0545

    labels * 10

--- mmaction/models/recognizers/recognizer3d.py
+++ mmaction/models/recognizers/recognizer3d.py
@@ -22,6 +22,7 @@ class Recognizer3D(BaseRecognizer):
             x, loss_aux = self.neck(x, labels.squeeze())
             losses.update(loss_aux)
         print("labels",labels)
+        print("\t"*10)
     
         cls_score= self.cls_head(x[0])
         i_score = self.cls_head(x[1])
